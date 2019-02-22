a42e4d7ac796f71edb1549d22c63b7b6bb5834b8
Adam Felix <adam.tyler.felix@gmail.com>
2019-02-22 17:49:48 -0500

    Exercism:  Matrix
    - Upgrade to new version of exercise

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,12 +1,13 @@
 class Matrix(object):
     def __init__(self, matrix_string):
         self.rows = self.parse(matrix_string)
+        self.columns = list(map(list, zip(*self.rows)))
     
     def parse(self, matrix_string):
         return [list(map(int, row.split())) for row in matrix_string.split('\n')]
 
     def row(self, index):
-        return self.rows[index]
+        return self.rows[index - 1]
 
     def column(self, index):
-        return list(self.rows[j][index] for j in range(len(self.rows)))
+        return self.columns[index - 1]
