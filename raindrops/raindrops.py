d5f58f1508c6c9edc2a608640162cd09515ab27d
Adam Felix <adam.tyler.felix@gmail.com>
2019-03-11 23:06:43 -0400

    Exercism:  Raindrops Iteration 4

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,4 +1,4 @@
-mods_and_sounds = [(3, 'Pling'), (5, 'Plang'), (7, 'Plong')]
+mods_and_sounds = ((3, 'Pling'), (5, 'Plang'), (7, 'Plong'))
 
 def raindrops(number):
     sounds = [sound for mod, sound in mods_and_sounds if number % mod == 0]
