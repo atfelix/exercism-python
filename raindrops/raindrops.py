826dadf9225172d8999457860fae226ff7fc028b
Adam Felix <adam.tyler.felix@gmail.com>
2019-03-11 22:51:56 -0400

    Exercism:  Raindrops Iteration 2

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,9 +1,13 @@
-sounds = {
-    3: 'Pling',
-    5: 'Plang',
-    7: 'Plong'
-}
-
 def raindrops(number):
-    matches = [mod for mod in sounds.keys() if number % mod == 0]
-    return ''.join(sounds[match] for match in matches) if matches else f'{number}'
+    matches = [mod for mod in [3, 5, 7] if number % mod == 0]
+    return ''.join(map(soundsFor, matches)) if matches else f'{number}'
+
+def soundsFor(mod):
+    if mod == 3:
+        return 'Pling'
+    elif mod == 5:
+        return 'Plang'
+    elif mod == 7:
+        return 'Plong'
+    else:
+        raise ValueError('Only 3, 5 and 7 are acceptable values for this function')
