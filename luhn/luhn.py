8ffe00f5b87a2ed9dcca73aebba6a09ffe09b137
Adam Felix <adam.tyler.felix@gmail.com>
2019-03-20 19:43:45 -0400

    Exercism:  Luhn (Iteration 2)

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -1,13 +1,11 @@
-from functools import reduce
-
 class Luhn(object):
     def __init__(self, card_num):
-        self.luhn_validity = is_valid(card_num)
+        self.luhn_validity = is_valid_luhn(card_num)
 
     def is_valid(self):
         return self.luhn_validity
 
-def is_valid(card_num):
+def is_valid_luhn(card_num):
     if any(not (char.isdigit() or char.isspace()) for char in card_num):
         return False
 
@@ -21,11 +19,11 @@ def is_valid(card_num):
 def luhn_count(digits):
     count = 0
     for index, digit in enumerate(digits):
-        count = (count + luhn_lookup(index, digit)) % 10
-    return count
+        count += luhn_lookup(index, digit)
+    return count % 10
 
-def luhn_lookup(index, digit, luhn_lookup=(0, 2, 4, 6, 8, 1, 3, 5, 7, 9)):
+def luhn_lookup(index, digit, digit_lookup=(0, 2, 4, 6, 8, 1, 3, 5, 7, 9)):
     if index % 2 != 1:
         return digit
     else:
-        return luhn_lookup[digit]
+        return digit_lookup[digit]
